e021a007216f7ae3f7cbe7bd7c40ffe547eebe37
Lyle Barras <hello@theelementalweb.com>
2017-09-17 10:19:56 +0100

    Build sqlite Database and create two migrations

---
+++ database/migrations/2017_09_17_085007_create_product_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('ISBN')->unique();
+            $table->string('title');
+            $table->string('author');
+            $table->double('price');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
