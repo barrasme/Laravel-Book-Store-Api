8cd97d6d69d0e1067c39b8ab3f9275f594d3466f
Lyle Barras <hello@theelementalweb.com>
2017-09-17 12:43:30 +0100

    Set up the routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It is a breeze. Simply tell Lumen the URIs it should respond to
-| and give it the Closure to call when that URI is requested.
-|
-*/
-
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
+/*
+ * Prefix the routes group with the version of the API for future proofing...
+ */
+$router->group(['prefix' => '1.0'], function () use ($router) {
 
-$router->get('author', [ 'uses' => 'ProductController']);
+    $router->get( 'author/{author}', [ 'uses' => 'ProductController@show' ] );
+
+    $router->get( 'categories', [ 'uses' => 'CategoryController@index' ] );
+    $router->get( 'category/{category}', [ 'uses' => 'CategoryController@show' ] );
+
+    $router->get( 'author/{author}/category/{category}', [ 'uses' => 'ProductController@show' ] );
+
+    $router->post( 'book/new', [ 'uses' => 'ProductController@store' ] );
+
+
+});
